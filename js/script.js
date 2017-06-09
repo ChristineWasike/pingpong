3c080f8f2891367abe3a96e42520b347cedaf1b5
ChristineWasike <christinewasike22@gmail.com>
2017-06-09 17:43:31 +0300

    I added specs, license, and more information on my README

--- js/script.js
+++ js/script.js
@@ -3,34 +3,30 @@ var output = function(number) {
   var array = [];
   for (var index = 1; index <= number; index++) {
     if ((index % 15) === 0) {
-      array.push("PingPong");
+      array.push("Ping Pong");
     } else if ((index % 5) === 0) {
       array.push("Pong");
-    } else if (array[indices] % 3 === 0) {
+    }else if ((index % 3)=== 0) {
       array.push("Ping");
-
-    } else {
-      return array.push(index);
-
-    }
+    }else
+      array.push(index);
   }
-
+  console.log(index);
+  return array;
 };
-alert(array);
-};
-
-
-
-
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
   $("form#game").submit(function(event) {
-    event.preventDefault();
-    var number = parseInt($("input#numeral").val());
-    var aftermath = output(number);
-    $("#results").text(number);
+    $("#results").empty();
 
+
+    var number = parseInt($("input#numeral").val());
+    var array = output(number);
+    array.forEach(function(aftermath) {
+      $("#results").append("<li>" + aftermath + "</li>");
+      event.preventDefault();
+    });
 
   });
 });
